acbd425f153748f7c4c2aa260930e6090b2b68b0
Skitterling <wyatt.kipp@gmail.com>
2026-01-16 17:21:58 -0600

    Add files via upload

--- Windows/CCDCsetup.ps1
+++ Windows/CCDCsetup.ps1
@@ -4,7 +4,13 @@ $UserAccount | Set-LocalUser -Password $Password
 Get-ScheduledTask | Stop-ScheduledTask
 Get-ScheduledTask | Disable-ScheduledTask
 
-$servicesToStop = @("CertPropSvc", "DiagTrack", "PlugPlay", "Spooler")
+$servicesToStop = @("CertPropSvc", "DiagTrack", "PlugPlay", "Spooler", "WinRM")
 Get-Service -Name $servicesToStop | Stop-Service -Force -PassThru | Set-Service -StartupType Disabled
+Disable-PSRemoting -Force
+Set-ItemProperty -Path 'HKLM:\System\CurrentControlSet\Control\Terminal Server' -Name "fDenyTSConnections" -Value 1
+Disable-NetFirewallRule -DisplayGroup "Remote Desktop" 
+
+
+Get-NetFirewallProfile | Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled True
 
 Read-Host -Prompt "Press Enter to exit"
