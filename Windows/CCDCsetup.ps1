711e713a8659b1e176bea417b209b9ceabb7872e
Skitterling <wyatt.kipp@gmail.com>
2026-01-16 16:03:34 -0600

    Add files via upload

--- Windows/CCDCsetup.ps1
+++ Windows/CCDCsetup.ps1
@@ -1,16 +1,10 @@
-Host$Password = Read-Host -AsSecureString
-New-LocalUser "FatCat" -Password $Password -FullName "FatCat The First"
-Add-LocalGroupMember -Group "Administrators" -Member "FatCat"
-
 $UserAccount = Get-LocalUser -Name "Administrator"
 $UserAccount | Set-LocalUser -Password $Password
-Disable-LocalUser -Name "Administrator"
 
 Get-ScheduledTask | Stop-ScheduledTask
 Get-ScheduledTask | Disable-ScheduledTask
 
-$servicesToStop = @("CertPropSvc", "DiagTrack", "MSDTC", "MSMQ", "PlugPlay", "Spooler", "UsoSvc", "TokenBroker")
-Get-Service -Name $servicesToStop | Set-Service -StartupType Automatic -PassThru | Start-Service -Force
+$servicesToStop = @("CertPropSvc", "DiagTrack", "PlugPlay", "Spooler")
+Get-Service -Name $servicesToStop | Stop-Service -Force -PassThru | Set-Service -StartupType Disabled
 
 Read-Host -Prompt "Press Enter to exit"
-
