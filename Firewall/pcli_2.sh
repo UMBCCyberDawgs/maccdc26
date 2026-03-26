b08770ee076117b6733b7454ac67867b0a89bc09
LMN1460 <127682574+LMN1460@users.noreply.github.com>
2026-03-26 18:00:43 -0400

    Update pcli_2.sh

--- Firewall/pcli_2.sh
+++ Firewall/pcli_2.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 read VAR_STDIN
-ssh admin@172.16.101.254 << EOF > pcli1_out.txt
+ssh admin@172.16.102.254 << EOF > pcli2_out.txt
 set cli scripting-mode on
 set cli config-output-format set
 $VARSTDIN
