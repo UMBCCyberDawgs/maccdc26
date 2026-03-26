430b29489b776ddc60e3ce1d1cb35ab31e2e4717
Skitterling <wyatt.kipp@gmail.com>
2026-03-26 14:23:42 -0400

    Add files via upload

---
+++ Windows/ManySearch.ps1
@@ -0,0 +1,9 @@
+#Search persistience
+#Services
+Write-Host "Weird Services:"
+Compare-Object -ReferenceObject (Get-Service | Select-Object -ExpandProperty Name | % { $_ -replace "_[0-9a-f]{2,8}$" } ) -DifferenceObject (gci -path hklm:\system\currentcontrolset\services | % { $_.Name -Replace "HKEY_LOCAL_MACHINE\\","HKLM:\" } | ? { Get-ItemProperty -Path "$_" -name objectname -erroraction 'ignore' } | % { $_.substring(40) }) -PassThru | ?{$_.sideIndicator -eq "=>"}
+#Search Misconfigs
+
+#Search Rootkits
+
+#Search Backdoors
