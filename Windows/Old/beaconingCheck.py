3d28f3111a096250df4404b8252f641034e7d837
takumi <alexanderrhenning@gmail.com>
2026-03-26 15:54:37 -0400

    ballin

---
+++ Windows/Old/beaconingCheck.py
@@ -0,0 +1,8 @@
+
+def main():
+    print("UDP Beaconing Malware Detector")
+    print("Listening for suspicious UDP traffic...\n")
+    print("No beaconing found")
+
+if __name__ == "__main__":
+    main()
--- Windows/beaconingCheck.py
+++
@@ -1,8 +0,0 @@
-
-def main():
-    print("UDP Beaconing Malware Detector")
-    print("Listening for suspicious UDP traffic...\n")
-    print("No beaconing found")
-
-if __name__ == "__main__":
-    main()
